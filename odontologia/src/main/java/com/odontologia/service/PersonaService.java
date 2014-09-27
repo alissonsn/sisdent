9688e5d7339e746253d69f23fd93e2e2572e0191
Arturo <Arturo@Atflorest>
2014-09-27 14:34:09 -0500

    consultar informacion personal Movil(funciona)

--- odontologia/src/main/java/com/odontologia/service/PersonaService.java
+++ odontologia/src/main/java/com/odontologia/service/PersonaService.java
@@ -5,6 +5,7 @@ import com.odontologia.model.Persona;
 public interface PersonaService {
 
 	public boolean registrarPersona(Persona persona);
+	public boolean actualizarPersona(Persona persona);
 	public Persona buscarPorUsuario(String usuario);
 	
 }
--- odontologia/src/main/java/com/odontologia/service/PersonaServiceImpl.java
+++ odontologia/src/main/java/com/odontologia/service/PersonaServiceImpl.java
@@ -1,14 +1,20 @@
 package com.odontologia.service;
 
+import javax.faces.event.ActionEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.primefaces.context.RequestContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.odontologia.model.Apoderado;
+import com.odontologia.model.Distrito;
+import com.odontologia.model.Paciente;
 import com.odontologia.model.Persona;
 import com.odontologia.model.Usuario;
+import com.odontologia.util.StaticHelp;
 
 @Service
 public class PersonaServiceImpl implements PersonaService{
@@ -30,6 +36,20 @@ public class PersonaServiceImpl implements PersonaService{
 		return resultado;
 	}
 
+	@Transactional
+	public boolean actualizarPersona(Persona persona) {
+		boolean resultado=false;
+		try{										
+			em.merge(persona);
+			resultado = true;
+		}
+		catch(Exception e){
+			System.out.println("ERROR: "+e.getMessage());
+			resultado = false;
+		}		
+		return resultado;
+	}
+	
 	@Transactional
 	public Persona buscarPorUsuario(String usuario) {		
 		Persona persona = new Persona();
