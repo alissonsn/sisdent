e6946b81b203b54b65fa8fef55b9f657e06c221a
Arturo <Arturo@Atflorest>
2014-10-02 11:33:46 -0500

    avanze consulinformacionPaciente, problema distrito :S

--- odontologia/src/main/java/com/odontologia/bean/PersonaBean.java
+++ odontologia/src/main/java/com/odontologia/bean/PersonaBean.java
@@ -1,11 +1,17 @@
 package com.odontologia.bean;
 
+import java.util.List;
+
+import javax.faces.event.ActionEvent;
 import javax.servlet.http.HttpSession;
 
+import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.odontologia.model.Distrito;
 import com.odontologia.model.Persona;
+import com.odontologia.model.Usuario;
 import com.odontologia.service.PersonaService;
 import com.odontologia.util.StaticHelp;
 
@@ -16,11 +22,13 @@ public class PersonaBean {
 	PersonaService personaService;
 	
 	private Persona persona;
-
+	private Distrito distrito;
+	private Usuario usuario;
+	
 	public PersonaBean() {
 		persona = new Persona();
-		
-		
+		distrito = new Distrito();
+		usuario = new Usuario();
 	}
 	
 	public void actualizarPersonaMovil(){
@@ -35,7 +43,15 @@ public class PersonaBean {
 		
 	}
 	
-	
+	public void actualizarPersonaWeb(ActionEvent actionEvent){
+		HttpSession session = StaticHelp.getSession();
+		persona = (Persona) session.getAttribute("personaSesion");	
+		if (personaService.actualizarPersona(persona)) {
+			StaticHelp.correctMessage(
+					"Se ha actualizado con éxito el paciente", "");
+			RequestContext.getCurrentInstance().update("frmDatos:growl");
+		}		
+	}
 	
 
 	public Persona getPersona() {
@@ -46,4 +62,19 @@ public class PersonaBean {
 		this.persona = persona;
 	}
 
+	public Distrito getDistrito() {
+		return distrito;
+	}
+
+	public void setDistrito(Distrito distrito) {
+		this.distrito = distrito;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
